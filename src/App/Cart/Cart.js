f1c8a04ca224289236bd45ae2617045136f3ee07
Shahar Shwartz <ravidar10@gmail.com>
2019-09-09 20:44:35 +0300

    changes

--- src/App/Cart/Cart.js
+++ src/App/Cart/Cart.js
@@ -13,32 +13,74 @@ class Cart extends React.Component {
 	}
 
 	componentDidMount() {
-		let ids = CartService.getAll().map(product => product.id);
+		this.loadCart();
+	}
+
+	loadCart() {
+		const cartProducts = CartService.getAll();
+		const ids = cartProducts.map(product => product.id);
 		ProductService.getByIds(ids)
 			.then(res => res.json())
-			.then(products => this.setState({products}))
-			.catch(error => console.log(error));
+			.then(products => {
+				products = this.addQuantities(products, cartProducts);
+				this.setState({ products });
+			});
 	}
+
+	addQuantities(products, cartProducts) {
+		let cartObj = {};
+		cartProducts.forEach(product => cartObj[product.id] = product.qty);
+		products.forEach(product => product.qty = cartObj[product.id]);
+		return products;
+	}
+
+	calcTotal(products) {
+		let total = 0;
+		products.forEach(product => {
+			total += product.qty * product.price;
+		});
+		return total;
+	}
+
+	remove(productId) {
+		CartService.remove(productId);
+		this.loadCart();
+	}
+
 
 	render() {
 		return (
 			<div>
 				<h1>Cart</h1>
-				<table>
+				<table className="table table-striped">
 					<thead>
 						<tr>
 							<th>ID</th>
+							<th>Price</th>
 							<th>Quantity</th>
+							<th>Sub-total</th>
+							<th> </th>
 						</tr>
 					</thead>
 					<tbody>
 						{this.state.products.map((product, index) => {
 							return <tr key={index}>
-								<td>{product.id}</td>
-								<td>{product.qty}</td>
+								<td><strong>{product.title}</strong></td>
+								<td>${product.price.toFixed(2)}</td>
+								<td><sub>x</sub>{product.qty}</td>
+								<td>${(product.qty * product.price).toFixed(2)}</td>
+								<td><button onClick={this.remove.bind(this, product.id)} className="btn btn-danger">X</button></td>
 							</tr>
 						})}
 					</tbody>
+					<tfoot>
+						<tr>
+							<td colSpan="3"> </td>
+							<td colSpan="2">
+								<strong>Total:</strong> ${this.calcTotal(this.state.products).toFixed(2)}
+							</td>
+						</tr>
+					</tfoot>
 				</table>
 			</div>
 		);
